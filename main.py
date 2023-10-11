d61290dff0fe7319744051e97992b8bfee6046b6
nguyen-brat <julius.2003.no.2@gmail.com>
2023-10-11 16:32:29 +0700

    x

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from run import Inference
 
 if __name__ == '__main__':
     inference = Inference()
-    st.title('Retrieval rerank generate demo')
+    st.title('Chatbot Demo')
 
     header = st.container()
     with header:
         question = st.text_input('Câu hỏi')
-        answer, information = inference(question)
+        information = inference(question)
         st.write('Câu trả lời là:')
-        st.write(f'{answer}')
+        #st.write(f'{answer}')
         st.header('Các tài liệu liên quan được tìm thấy:', divider='rainbow')
         first_doc, second_doc, third_doc = st.columns(3)
         with first_doc:
