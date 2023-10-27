30e52461008f29154b47838bae089ca4ae717b79
nguyen-brat <julius.2003.no.2@gmail.com>
2023-10-27 11:14:53 +0700

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         answer, information = inference(question)
         st.write('Câu trả lời là:')
         st.write(f'{answer}')
-        st.header('Các tài liệu liên quan được tìm thấy:', divider='rainbow')
+        # st.header('Các tài liệu liên quan được tìm thấy:', divider='rainbow')
         # first_doc, second_doc, third_doc = st.columns(3)
         # with first_doc:
         #     st.write(f'{information[0]}')
